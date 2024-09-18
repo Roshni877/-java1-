57a43c1f3e2742bdb75c5dcf3a871e09e16749ea
Roshni <roshni.23cs122@sode-edu.in>
2024-09-19 00:54:02 +0530

    Update R3.java

--- R3.java
+++ R3.java
@@ -1,11 +1,11 @@
 public class R3{
-public static void main(String[] args){
-for(int i=3;i<=20;i++){
+    public static void main(String[] args){
+       for(int i=3;i<=20;i++){
 System.out.println("Multiplication table of "+i);
-for(int j=1;j<=10;j++){
+      for(int j=1;j<=10;j++){
 System.out.println(i+"*"+j+"="+(i*j));
 }
 System.out.println();
-}
-}
+        }
+    }
 }
