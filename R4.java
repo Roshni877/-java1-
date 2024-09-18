87f0d284f066c06480958a82c0a7389f8b2a5a6f
Roshni <roshni.23cs122@sode-edu.in>
2024-09-19 01:00:55 +0530

    Update R4.java

--- R4.java
+++ R4.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 public class R4{
-public static void main(String[] args){
-Scanner scanner=new Scanner(System.in);
-System.out.print("Enter the start value:");
-int start=scanner.nextInt();
-System.out.print("Enter the end value:");
-int end=scanner.nextInt();
-scanner.close();
-for(int i=start;i<=end;i++){
-System.out.println("Multiplication Table for "+i);
-for(int j=1;j<=10;j++){
-System.out.println(i+"x"+j+"="+(i*j));
-}
-System.out.println();
-}
-}
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the start value:");
+        int start=scanner.nextInt();
+        System.out.print("Enter the end value:");
+        int end=scanner.nextInt();
+        scanner.close();
+        for(int i=start;i<=end;i++){
+        System.out.println("Multiplication Table for "+i);
+        for(int j=1;j<=10;j++){
+        System.out.println(i+"x"+j+"="+(i*j));
+   }
+        System.out.println();
+      }
+   }
 }
