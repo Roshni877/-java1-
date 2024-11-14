8f4c49c75c5c05ba233285baaaa2935a76acd3fa
Roshni <roshni.23cs122@sode-edu.in>
2024-11-14 10:00:48 +0530

    Implementation of Tools and TestTools classes in Java

---
+++ Tools.java
@@ -0,0 +1,39 @@
+package pkg1;
+public class Tools{
+    public void numbers(int n1){
+        for(int i=0;i<=n1;i++)
+        {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public void even(int n){
+        for(int i=0;i<=n;i+=2)
+        {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public void odd(int n){
+        for(int i=1;i<=n;i+=2){
+            System.out.print(i+" ");}
+        System.out.println();
+    }
+    public void mult3(){
+        for(int i=0;i<=30;i+=3){
+            System.out.print(i+" ");}
+            System.out.println();
+    }
+    public void mult4(){
+        for(int i=0;i<=40;i+=4){
+            System.out.print(i+" ");}
+        System.out.println();
+        }
+    public void mult5(){
+        for(int i=0;i<=50;i+=5){
+            System.out.print(i+" ");}
+        System.out.println();
+        }
+    }
+    
+}
