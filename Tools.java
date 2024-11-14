ccadd134c77f817c69b041b8f06daa484c22bfd0
Roshni <roshni.23cs122@sode-edu.in>
2024-11-14 10:48:52 +0530

    Update Tools.java

--- Tools.java
+++ Tools.java
@@ -8,30 +8,30 @@ public class Tools{
         System.out.println();
     }
     public void even(int n){
-        for(int i=0;i<=n;i+=2)
+        for(int i=0;i<=n;i++)
         {
-            System.out.print(i+" ");
+            System.out.print(i*2+" ");
         }
         System.out.println();
     }
     public void odd(int n){
-        for(int i=1;i<=n;i+=2){
-            System.out.print(i+" ");}
+        for(int i=1;i<=n;i++){
+            System.out.print(2*i+1+" ");}
         System.out.println();
     }
     public void mult3(){
-        for(int i=0;i<=n;i+=3){
-            System.out.print(i+" ");}
+        for(int i=0;i<=n;i++){
+            System.out.print(i*3+" ");}
             System.out.println();
     }
     public void mult4(){
-        for(int i=0;i<=n;i+=4){
-            System.out.print(i+" ");}
+        for(int i=0;i<=n;i++){
+            System.out.print(4*i+" ");}
         System.out.println();
         }
     public void mult5(){
-        for(int i=0;i<=n;i+=5){
-            System.out.print(i+" ");}
+        for(int i=0;i<=n;i++){
+            System.out.print(5*i+" ");}
         System.out.println();
         }
     }
