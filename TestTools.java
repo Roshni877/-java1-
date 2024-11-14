163f740b1a35e7923fbb8ff3b5464f022747ddf3
Roshni <roshni.23cs122@sode-edu.in>
2024-11-14 10:33:30 +0530

    Update TestTools.java

--- TestTools.java
+++ TestTools.java
@@ -2,18 +2,11 @@ package pkg1;
 public class TestTools{
     public static void main(String[]args){
         Tools t1=new Tools();
-        System.out.println("numbers(10):");
         t1.numbers(10);
-        System.out.println("even(20):");
-        t1.even(20);
-        System.out.println("odd(19):");
-        t1.odd(19);
-        System.out.println("mult3:");
-        t1.mult3();
-        System.out.println("mult4:");
-        t1.mult4();
-        System.out.println("mult5:");
-        t1.mult5();
-        
+        t1.even(10);
+        t1.odd(10);
+        t1.mult3(10);
+        t1.mult4(10);
+        t1.mult5(10);
     }
 }
