5503a901da194ba4cec55ade8a14dd001817b4fa
Roshni <roshni.23cs122@sode-edu.in>
2024-11-14 10:34:39 +0530

    Update Tools.java

--- Tools.java
+++ Tools.java
@@ -20,17 +20,17 @@ public class Tools{
         System.out.println();
     }
     public void mult3(){
-        for(int i=0;i<=30;i+=3){
+        for(int i=0;i<=n;i+=3){
             System.out.print(i+" ");}
             System.out.println();
     }
     public void mult4(){
-        for(int i=0;i<=40;i+=4){
+        for(int i=0;i<=n;i+=4){
             System.out.print(i+" ");}
         System.out.println();
         }
     public void mult5(){
-        for(int i=0;i<=50;i+=5){
+        for(int i=0;i<=n;i+=5){
             System.out.print(i+" ");}
         System.out.println();
         }
